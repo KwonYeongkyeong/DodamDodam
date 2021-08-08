d3d0dc5a8007c0cd013ee5a9b046ced379c0a776
lhj3974 <86765555+lhj3974@users.noreply.github.com>
2021-08-08 23:52:46 +0900

    paint 버튼 이름 한글로 수정
    .

--- dodamdodam/dodam/static/app.js
+++ dodamdodam/dodam/static/app.js
@@ -54,10 +54,10 @@ function handleRangeChange(event) {
 function handleModeClick() {
   if (filling === true) {
     filling = false;
-    mode.innerText = "Fill";
+    mode.innerText = "채우기";
   } else {
     filling = true;
-    mode.innerText = "Paint";
+    mode.innerText = "그리기";
   }
 }
 
