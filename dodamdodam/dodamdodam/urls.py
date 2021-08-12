e1a66b32537e15c2f2bce132c6c80e371e6bb7f7
shin-jisong <songjin1127@naver.com>
2021-08-13 02:07:30 +0900

    수정

--- dodamdodam/dodamdodam/urls.py
+++ dodamdodam/dodamdodam/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('dodam/',include('dodam.urls')),
     path('setQ/', views.setQ, name='setQ')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
